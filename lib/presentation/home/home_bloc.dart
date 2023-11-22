2e052bfbdcd63f9de2f1d5b75cbefcdf6a1889d9
phamtanphat <phatdroid94@gmail.com>
2023-11-22 21:30:59 +0700

    execute request

--- lib/presentation/home/home_bloc.dart
+++ lib/presentation/home/home_bloc.dart
@@ -1,10 +1,29 @@
+import 'package:flutter_weather_forcast29092023/base/base_bloc.dart';
+import 'package:flutter_weather_forcast29092023/base/base_event.dart';
 import 'package:flutter_weather_forcast29092023/data/repository/weather_repository.dart';
+import 'package:flutter_weather_forcast29092023/presentation/home/home_event.dart';
 
-class HomeBloc {
+class HomeBloc extends BaseBloc{
   WeatherRepository? _weatherRepository;
 
   void setWeatherRepository(WeatherRepository weatherRepository) {
     _weatherRepository = weatherRepository;
   }
 
+  @override
+  void dispatch(BaseEvent event) {
+    switch(event.runtimeType) {
+      case GetWeatherFromCityEvent:
+        _requestWeatherFromCityName(event as GetWeatherFromCityEvent);
+        break;
+    }
+  }
+
+  void _requestWeatherFromCityName(GetWeatherFromCityEvent event) {
+      _weatherRepository?.executeGetWeatherFromCity(event.cityName)
+          .then((searchWeatherFromCityDTO) {
+
+          })
+          .catchError((error) => print(error));
+  }
 }
--- lib/presentation/home/home_event.dart
+++ lib/presentation/home/home_event.dart
@@ -1,1 +1,11 @@
-abstract class HomeEventBase { }
+import 'package:flutter_weather_forcast29092023/base/base_event.dart';
+
+class GetWeatherFromCityEvent extends BaseEvent {
+  String cityName;
+
+  GetWeatherFromCityEvent(this.cityName);
+
+  @override
+  List<Object?> get props => [];
+
+}
